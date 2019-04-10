95d04d671abd7cde5f56eb730ebbc3e590590563
Tanner Eustice <teustice@msn.com>
2019-04-10 13:29:42 -0700

    comment available image sizes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var app = express();
 
 const scrapeCl = (url, callback) => {
-  request(url, function(error, response, html){
-    if(!error){
+  request(url, function(error, response, html) {
+    if (!error) {
       var $ = cheerio.load(html);
 
       var title, release, rating;
@@ -14,25 +14,26 @@ const scrapeCl = (url, callback) => {
         vans: []
       };
 
-      // We'll use the unique header class as a starting point.
-
-      $('.result-row').each(function(index, van){
+      $('.result-row').each(function(index, van) {
 
         let imageData = $(van).find('.result-image').attr('data-ids')
         imageData = imageData.split(':')
         totalImageCount = imageData.unshift();
 
-        //Parse the id
+        //Parse the image id's from the CL gallery
         imageData.forEach(function(id, i) {
           let idArray = id.split(',');
           idArray = idArray.slice(0, 1)
           imageData[i] = idArray[0]
         })
 
-        imageData = imageData.slice(1,imageData.length) //remove leading 1
+        imageData = imageData.slice(1, imageData.length) //remove leading 1
 
         let images = imageData.map(function(id) {
-          return(
+          //Known supported resolutions are:
+          //300x300
+          //600x450
+          return (
             `https://images.craigslist.org/${id}_600x450.jpg`
           )
         })
@@ -57,12 +58,12 @@ const scrapeCl = (url, callback) => {
   })
 }
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 
   let paths = []
 
-  app._router.stack.forEach(function(r){
-    if (r.route && r.route.path){
+  app._router.stack.forEach(function(r) {
+    if (r.route && r.route.path) {
       paths.push(`<a href=${r.route.path}><h3>${r.route.path}</h3></a>`);
     }
   })
@@ -71,21 +72,21 @@ app.get('/', function(req, res){
 
 })
 
-app.get('/hiace', function(req, res){
+app.get('/hiace', function(req, res) {
   url = 'https://portland.craigslist.org/search/cta?query=toyota+hiace&sort=rel';
   scrapeCl(url, function(json) {
     res.json(json)
   })
 })
 
-app.get('/townace', function(req, res){
+app.get('/townace', function(req, res) {
   url = 'https://portland.craigslist.org/search/cta?query=toyota+townace&sort=rel';
   scrapeCl(url, function(json) {
     res.json(json)
   })
 })
 
-app.get('/liteace', function(req, res){
+app.get('/liteace', function(req, res) {
   url = 'https://portland.craigslist.org/search/cta?query=toyota+liteace&sort=rel';
   scrapeCl(url, function(json) {
     res.json(json)
