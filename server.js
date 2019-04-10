8d9646214ed1b88d151530f81c197d441b1026d9
Tanner Eustice <teustice@msn.com>
2019-04-10 13:20:38 -0700

    remove gitignored files

--- server.js
+++ server.js
@@ -93,5 +93,5 @@ app.get('/liteace', function(req, res){
 })
 
 app.listen('8081')
-console.log('Magic happens on port 8081');
+console.log('Ready to rumble on port 8081');
 exports = module.exports = app;
